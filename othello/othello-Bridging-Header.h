8c9d6c94a900e3b70174c658a88ee421abd7a3b7
sasa8810 <hayato.corocoro@gmail.com>
2024-03-21 00:20:35 +0900

    a

---
+++ othello/othello-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  othello-Bridging-Header.h
+//  othello
+//
+//  Created by sasa on 2024/03/21.
+//
+
+#ifndef othello_Bridging_Header_h
+#define othello_Bridging_Header_h
+
+#include "reversi.h"
+
+#endif /* othello_Bridging_Header_h */
