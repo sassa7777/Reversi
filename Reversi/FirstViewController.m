225ee90213b8cfd873488b04a1b5810e8b9c0955
ささ <91324887+sassa7777@users.noreply.github.com>
2024-05-17 16:59:13 +0900

    Update FirstViewController.m

--- Reversi/FirstViewController.m
+++ Reversi/FirstViewController.m
@@ -21,6 +21,30 @@
 }
 
 - (void)select_strength:(NSSegmentedControl *)sender __attribute__((ibaction)) {
+    switch (sender.selectedSegment) {
+        case 1:
+            firstDEPTH = 1;
+            _strength_txt.stringValue = @"初心者向け";
+            break;
+        case 2:
+            firstDEPTH = 2;
+            _strength_txt.stringValue = @"初心者よりはできる人向け";
+            break;
+        case 3:
+            firstDEPTH = 4;
+            _strength_txt.stringValue = @"ある程度できる人向け";
+            break;
+        case 4:
+            firstDEPTH = 6;
+            _strength_txt.stringValue = @"少し自信がある人向け";
+            break;
+        case 5:
+            firstDEPTH = 10;
+            _strength_txt.stringValue = @"自信がある人向け";
+            break;
+        default:
+            break;
+    }
 }
 
 - (void)tapstart:(id)sender __attribute__((ibaction)) {
