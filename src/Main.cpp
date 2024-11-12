6324f38e2dc951641b73c02a0772d801258eaeaa
Takuto Yamana <nyanyama3141pi2@yahoo.co.jp>
2024-11-13 02:38:46 +0900

    update popcount

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,6 +10,14 @@ using App = SceneManager<String>;
 #define RESOURCE_PATH U""
 #endif
 
+// builtin functions
+#ifdef __GNUC__
+    #define	popcountll(x) popcountll(x)
+#else
+    #define	popcountll(x) __popcnt64(x)
+#endif
+// if you are using C++20, you can just use std::popcount(x)
+
 int stone_edge, stone_size;
 
 void DrawBoard() {
@@ -141,11 +149,11 @@ void Main()
             if (isFinished()) {
                 game_status = 3;
                 if (nowTurn == BLACK_TURN) {
-                    black_stone_count = __builtin_popcountll(b.playerboard);
-                    white_stone_count = __builtin_popcountll(b.opponentboard);
+                    black_stone_count = popcountll(b.playerboard);
+                    white_stone_count = popcountll(b.opponentboard);
                 } else {
-                    white_stone_count = __builtin_popcountll(b.playerboard);
-                    black_stone_count = __builtin_popcountll(b.opponentboard);
+                    white_stone_count = popcountll(b.playerboard);
+                    black_stone_count = popcountll(b.opponentboard);
                 }
                 if ((botplayer == 0 && black_stone_count > white_stone_count) || (botplayer == 1 && white_stone_count > black_stone_count)) {
                     winner = 0;
